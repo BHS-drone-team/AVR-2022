899d1d92fd3b98974477af3b09c57caaf0c30fbb
BHS_drone_team <jglaw2023@gmail.com>
2022-10-11 09:21:20 -0400

    commented out things we dont want in sandbox

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -38,7 +38,7 @@ class Sandbox(MQTTModule):
         # find the associated capital. However, this does not work in reverse. So here,
         # we're creating a dictionary of MQTT topics, and the methods we want to run
         # whenever a message arrives on that topic.
-        self.topic_map = {"avr/fcm/velocity": self.show_velocity}
+        #self.topic_map = {"avr/fcm/velocity": self.show_velocity}
 
         # TESTING SENSING AN APRIL TAG MESSAGE FROM MQTT
         self.topic_map = {"avr/apriltags/visible": self.show_april_tag_detected}
@@ -53,11 +53,11 @@ class Sandbox(MQTTModule):
     # This is what executes whenever a message is received on the "avr/fcm/velocity"
     # topic. The content of the message is passed to the `payload` argument.
     # The `AvrFcmVelocityMessage` class here is beyond the scope of AVR.
-    def show_velocity(self, payload: AvrFcmVelocityPayload) -> None:
-        vx = payload["vX"]
-        vy = payload["vY"]
-        vz = payload["vZ"]
-        v_ms = (vx, vy, vz)
+    #def show_velocity(self, payload: AvrFcmVelocityPayload) -> None:
+    #    vx = payload["vX"]
+    #    vy = payload["vY"]
+    #    vz = payload["vZ"]
+    #    v_ms = (vx, vy, vz)
 
         # Use methods like `debug`, `info`, `success`, `warning`, `error`, and
         # `critical` to log data that you can see while your code runs.
@@ -65,22 +65,22 @@ class Sandbox(MQTTModule):
         # This is what is known as a "f-string". This allows you to easily inject
         # variables into a string without needing to combine lots of strings together.
         # https://realpython.com/python-f-strings/#f-strings-a-new-and-improved-way-to-format-strings-in-python
-        logger.debug(f"Velocity information: {v_ms} m/s")
+    #    logger.debug(f"Velocity information: {v_ms} m/s")
 
 
 
     # Here is an example on how to publish a message to an MQTT topic to
     # perform an action
-    def open_servo(self) -> None:
+    #def open_servo(self) -> None:
         # It's super easy, use the `self.send_message` method with the first argument
         # as the topic, and the second argument as the payload.
         # Pro-tip, if you set `python.analysis.typeCheckingMode` to `basic` in you
         # VS Code preferences, you'll get a red underline if your payload doesn't
         # match the expected format for the topic.
-        self.send_message(
-            "avr/pcm/set_servo_open_close",
-            {"servo": 0, "action": "open"},
-        )
+        #self.send_message(
+        #    "avr/pcm/set_servo_open_close",
+        #    {"servo": 0, "action": "open"},
+        #)
 
 
 if __name__ == "__main__":
