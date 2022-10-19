ea96d5998669650c730c83278cd7a21549e998e6
BHS_drone_team <jglaw2023@gmail.com>
2022-10-19 14:41:01 -0400

    ahhh

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -61,7 +61,7 @@ class Sandbox(MQTTModule):
                     "avr/pcm/set_servo_open_close",
                     {"servo": 0, "action": "open"},
                 )
-                time.sleep(1)
+                time.sleep(10)
 #                self.send_message(
 #                    "avr/pcm/set_temp_color",
 #                    {"wrgb": (255, 255, 0, 0), "time": 0.5}
