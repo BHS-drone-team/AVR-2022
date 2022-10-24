05d7a1eed7886d75f1e3ef723bb5f8ce8427466f
BHS_drone_team <jglaw2023@gmail.com>
2022-10-24 11:56:09 -0400

    reset everything and started over

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -1,3 +1,4 @@
+from typing_extensions import Self
 from bell.avr.mqtt.client import MQTTModule
 from bell.avr.mqtt.payloads import (
     AvrApriltagsVisiblePayload,
@@ -9,8 +10,14 @@ import time
 
 class Sandbox(MQTTModule):
     HORIZ_DROP_TOLERANCE = 20.0 # Tolerance for dropping water autonomously in cm NOTE needs to be tuned
+    global visible_tag
     visible_tag = None # Most currently seen april tag
+    global has_dropped
     has_dropped = False # Flag to only send drop command once per auto enable
+    global is_within_tolerance_variable
+    is_within_tolerance_variable = False
+    global auton_enable_final
+    auton_enable_final = False
     # NOTE needs logic to handle multiple drops per auto
     def __init__(self):
         super().__init__()
@@ -19,29 +26,39 @@ class Sandbox(MQTTModule):
 #NOTE this doesn't work because it doesn't send enable disable here        self.topic_map = {"avr/autonomous/disable" : self.on_autonomous_disable} # On auto disable in GUI, run autonomous_disable method
         self.topic_map = {"avr/apriltags/visible" : self.update_visible_tag} # On seeing an april tag, run update_visible_tag
 
-    # Run autonomous when enabled
-    def on_autonomous_enable(self, payload: AvrAutonomousEnablePayload):
-        # Check if there is a visible april tag, if the vehicle is within specified horizontal tolerance, and if the vehicle has not already dropped the water
-        if self.visible_tag != None and self.is_within_tolerance and (not self.has_dropped):
-            servo_0 = 0
+        if visible_tag == 0 and is_within_tolerance_variable and has_dropped == False and auton_enable_final:
+            self.go_for_auton(True)
+
+    def go_for_auton(self, good_to_go) -> None:
+        if good_to_go == True:
             self.open_servo(0) # Open servo on channel 0
             self.blink_leds(3, (255, 255, 0, 0), 0.5) # Blink LEDs 3 times at 0.5 second interval
-            has_dropped = True
+            global has_dropped
+            has_dropped == True
+
+    # Run autonomous when enabled
+    def on_autonomous_enable(self, payload: AvrAutonomousEnablePayload):
+        auton_enable = payload["enabled"]
+        if auton_enable == True:
+            global auton_enable_final
+            auton_enable_final = True
+        # Check if there is a visible april tag, if the vehicle is within specified horizontal tolerance, and if the vehicle has not already dropped the water
+
+
+
 
     # Update class variable visible_tag to the most currently seen tag and log the horizontal distance between the vehicle and april tag
     def update_visible_tag(self, payload: AvrApriltagsVisiblePayload):
         tag_list=payload["tags"] #this is to get the list out of the payload
-#        self.visible_tag = payload[0] # NOTE if no visible tags are seen, what is payload[0]? If it is None, update visible_tag to None
-        horiz_dist = tag_list[1] #pulls the horiz_dist from the tag list
-        logger.debug(f"Horizontal distance: {horiz_dist} cm") # NOTE need to check which logger method to use
+        april_id = tag_list[0]["id"]
+        tag_horiz_dist = tag_list[0]["horizontal_dist"] # Horizontal scalar distance from vehicle to tag in cm
+        if april_id == 0 and tag_horiz_dist > self.HORIZ_DROP_TOLERANCE:
+            global visible_tag
+            visible_tag = 0
+            global is_within_tolerance_variable
+            is_within_tolerance_variable = True
 
 
-    # Return whether the vehicle is within the desired horizontal tolerance of the april tag
-    def is_within_tolerance(self, payload: AvrApriltagsVisiblePayload):
-        tag_list=payload["tags"]
-        tag_horiz_dist = tag_list[1]["horizontal_dist"] # Horizontal scalar distance from vehicle to tag in cm
-        if tag_horiz_dist > self.HORIZ_DROP_TOLERANCE:
-            return True #i did this because it didn't like the variable defining
 
     # Open servo on desired channel
     def open_servo(self, channel):
