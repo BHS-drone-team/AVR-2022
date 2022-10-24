1e48969dd5a013e575e8242055fe062befbb687d
BHS_drone_team <jglaw2023@gmail.com>
2022-10-24 14:55:33 -0400

    another overhaul

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -9,45 +9,27 @@ import time
 
 class Sandbox(MQTTModule):
     HORIZ_DROP_TOLERANCE = 20.0 # Tolerance for dropping water autonomously in cm NOTE needs to be tuned
-#    global visible_tag
-#    visible_tag = None # Most currently seen april tag
-    global has_dropped
+    visible_tag = None # Most currently seen april tag
     has_dropped = False # Flag to only send drop command once per auto enable
-#    global is_within_tolerance_variable
-#    is_within_tolerance_variable = False
-#    global auton_enable_final
-#    auton_enable_final = False
+    is_within_tolerance_variable = False
+    auton_enable_final = False
     # NOTE needs logic to handle multiple drops per auto
     def __init__(self):
         super().__init__()
 
-#        self.topic_map = {"avr/autonomous/enable" : self.on_autonomous_enable} # On auto enable in GUI, run autonomous
+        self.topic_map = {"avr/autonomous/enable" : self.on_autonomous_enable} # On auto enable in GUI, run autonomous
 #NOTE this doesn't work because it doesn't send enable disable here        self.topic_map = {"avr/autonomous/disable" : self.on_autonomous_disable} # On auto disable in GUI, run autonomous_disable method
         self.topic_map = {"avr/apriltags/visible" : self.update_visible_tag} # On seeing an april tag, run update_visible_tag
 
-        if visible_tag == 0 and is_within_tolerance_variable and has_dropped == False:
-            self.go_for_auton(True)
-            logger.debug(f"has_dropped: {has_dropped}")
 
-    def go_for_auton(self, good_to_go) -> None:
-        if good_to_go == True:
-            self.open_servo(0) # Open servo on channel 0
-            self.blink_leds(3, (255, 255, 0, 0), 0.5) # Blink LEDs 3 times at 0.5 second interval
-            global has_dropped
-            has_dropped == True
-
-    # Run autonomous when enabled
-#    def on_autonomous_enable(self, payload: AvrAutonomousEnablePayload) -> None:
-#        auton_enable = payload["enabled"]
-#        logger.debug(f"auton_enable: {auton_enable}")
+    #Run autonomous when enabled
+    def on_autonomous_enable(self, payload: AvrAutonomousEnablePayload) -> None:
+        auton_enable = payload["enabled"]
+        logger.debug(f"auton_enable: {auton_enable}")
 #        if auton_enable == True:
 #            global auton_enable_final
 #            auton_enable_final = True
 #            logger.debug(f"auton_enable_final: {auton_enable_final}")
-        # Check if there is a visible april tag, if the vehicle is within specified horizontal tolerance, and if the vehicle has not already dropped the water
-
-
-
 
     # Update class variable visible_tag to the most currently seen tag and log the horizontal distance between the vehicle and april tag
     def update_visible_tag(self, payload: AvrApriltagsVisiblePayload):
@@ -63,27 +45,28 @@ class Sandbox(MQTTModule):
             is_within_tolerance_variable = True
             logger.debug(f"is_within_tolerance_variable: {is_within_tolerance_variable}")
 
-
-
-    # Open servo on desired channel
-    def open_servo(self, channel):
-        self.send_message(
+    if visible_tag == 0 and is_within_tolerance_variable:
+        code_recieved = True
+        logger.debug(f"code_recieved: {code_recieved}")
+        def open_servo(self):
+                self.send_message(
                     "avr/pcm/set_servo_open_close",
-                    {"servo": channel, "action": "open"}
+                    {"servo": 0, "action": "open"}
             )
-    # Close servo on desired channel
-    def close_servo(self, channel):
-        self.send_message(
-                    "avr/pcm/set_servo_open_close",
-                    {"servo": channel, "action": "close"}
-            )
-    # Blink led for desired iterations with desired wrbg value for specified time interval
-    def blink_leds(self, iterations, wrgb, time):
-        for _ in iterations:
-             self.send_message(
+        def blink_leds(self):
+                for _ in range(3):
+                    self.send_message(
                         "avr/pcm/set_temp_color",
-                        {"wrgb": wrgb, "time": time}
+                        {"wrgb": (255, 255, 0, 0), "time": 0.5}
                 )
+                    time.sleep(1)
+        def close_servo(self):
+                self.send_message(
+                    "avr/pcm/set_servo_open_close",
+                    {"servo": 0, "action": "close"}
+            )
+        code_ran=True
+        logger.debug(f"code_ran: {code_ran}")
 if __name__ == "__main__":
     box = Sandbox()
     box.run()
