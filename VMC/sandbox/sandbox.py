f55e55f4d3246efc84df318db913ef0ea3cfd7ba
BHS_drone_team <jglaw2023@gmail.com>
2022-10-24 15:52:04 -0400

    rebuild again

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -1,3 +1,9 @@
+# Here we import our own MQTT library which takes care of a lot of boilerplate
+# code related to connecting to the MQTT server and sending/receiving messages.
+# It also helps us make sure that our code is sending the proper payload on a topic
+# and is receiving the proper payload as well.
+#from typing_extensions import Self
+from ast import Not
 from bell.avr.mqtt.client import MQTTModule
 from bell.avr.mqtt.payloads import (
     AvrApriltagsVisiblePayload,
@@ -8,63 +14,62 @@ from loguru import logger
 import time
 
 class Sandbox(MQTTModule):
-    HORIZ_DROP_TOLERANCE = 20.0 # Tolerance for dropping water autonomously in cm NOTE needs to be tuned
-    has_dropped = False # Flag to only send drop command once per auto enable
-    auton_enable_final = False
-    visible_tag = None
-    is_within_tolerance_variable = False
+
+    HORIZ_DROP_TOLERANCE = 20 # Tolerance for dropping water autonomously in cm NOTE needs to be tuned
     # NOTE needs logic to handle multiple drops per auto
     def __init__(self):
         super().__init__()
 
         self.topic_map = {"avr/autonomous/enable" : self.on_autonomous_enable} # On auto enable in GUI, run autonomous
-#NOTE this doesn't work because it doesn't send enable disable here        self.topic_map = {"avr/autonomous/disable" : self.on_autonomous_disable} # On auto disable in GUI, run autonomous_disable method
+#        self.topic_map = {"avr/autonomous/disable" : self.on_autonomous_disable} # On auto disable in GUI, run autonomous_disable method
         self.topic_map = {"avr/apriltags/visible" : self.update_visible_tag} # On seeing an april tag, run update_visible_tag
+        self.visible_tag = None
+        self.has_dropped = False
+        self.HORIZ_DROP_TOLERANCE = 20 # Tolerance for dropping water autonomously in cm NOTE needs to be tuned
 
+    # Run autonomous when enabled
+    def on_autonomous_enable(self, payload: AvrAutonomousEnablePayload):
+        recieved_auton_enable = payload["enabled"]
+        logger.debug(f"recieved auton enable: {recieved_auton_enable}")
+        # Check if there is a visible april tag, if the vehicle is within specified horizontal tolerance, and if the vehicle has not already dropped the water
+        if self.visible_tag == 0 and self.has_dropped == False:
+            self.open_servo(0) # Open servo on channel 0
+            self.blink_leds(3, (255, 255, 0, 0), 0.5) # Blink LEDs 3 times at 0.5 second interval
+            self.has_dropped = True
+            logger.debug(f"self.has_dropped: {self.has_dropped}")
 
-    #Run autonomous when enabled
-    def on_autonomous_enable(self, payload: AvrAutonomousEnablePayload) -> None:
-        auton_enable = payload["enabled"]
-        logger.debug(f"auton_enable: {auton_enable}")
-#        if auton_enable == True:
-#            global auton_enable_final
-#            auton_enable_final = True
-#            logger.debug(f"auton_enable_final: {auton_enable_final}")
 
     # Update class variable visible_tag to the most currently seen tag and log the horizontal distance between the vehicle and april tag
     def update_visible_tag(self, payload: AvrApriltagsVisiblePayload):
         tag_list=payload["tags"] #this is to get the list out of the payload
-        april_id = tag_list[0]["id"]
-        tag_horiz_dist = tag_list[0]["horizontal_dist"] # Horizontal scalar distance from vehicle to tag in cm
-        if april_id == 0 and tag_horiz_dist < self.HORIZ_DROP_TOLERANCE:
-            global visible_tag
-            visible_tag = 0
-            global is_within_tolerance_variable
-            is_within_tolerance_variable = True
+        horiz_dist = tag_list[0]["horizontal_dist"]
+        tag_id = tag_list[0]["id"]
+        logger.debug(f"Horizontal distance: {horiz_dist} cm") # NOTE need to check which logger method to use
+        if horiz_dist < self.HORIZ_DROP_TOLERANCE and tag_id == 0:
+            self.visible_tag = 0
 
-    def 
-        if visible_tag == 0 and is_within_tolerance_variable:
-            code_recieved = True
-            logger.debug(f"code_recieved: {code_recieved}")
-            def open_servo(self):
-                self.send_message(
+    # Open servo on desired channel
+    def open_servo(self, channel):
+        self.send_message(
                     "avr/pcm/set_servo_open_close",
-                    {"servo": 0, "action": "open"}
+                    {"servo": channel, "action": "open"}
             )
-        def blink_leds(self):
-                for _ in range(3):
-                    self.send_message(
+
+    # Close servo on desired channel
+    def close_servo(self, channel):
+        self.send_message(
+                    "avr/pcm/set_servo_open_close",
+                    {"servo": channel, "action": "close"}
+            )
+
+    # Blink led for desired iterations with desired wrbg value for specified time interval
+    def blink_leds(self, iterations, wrgb, time):
+        for _ in iterations:
+             self.send_message(
                         "avr/pcm/set_temp_color",
-                        {"wrgb": (255, 255, 0, 0), "time": 0.5}
+                        {"wrgb": wrgb, "time": time}
                 )
-                    time.sleep(1)
-        def close_servo(self):
-                self.send_message(
-                    "avr/pcm/set_servo_open_close",
-                    {"servo": 0, "action": "close"}
-            )
-        code_ran=True
-        logger.debug(f"code_ran: {code_ran}")
+
 if __name__ == "__main__":
     box = Sandbox()
     box.run()
