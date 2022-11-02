93226383f531a9bc56a1beda40ab752c21849d49
BHS_drone_team <jglaw2023@gmail.com>
2022-11-02 13:12:28 -0400

    bruh stupid led values this is a fix

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -174,7 +174,7 @@ class Sandbox(MQTTModule):
 
     # Blink led for desired iterations with desired wrbg value for specified time interval
     def blink_leds(self, time):
-        wrgb = (0,51,255,0)
+        wrgb = (0,0,51,255)
         self.send_message(
                     "avr/pcm/set_temp_color",
                     {"wrgb": wrgb, "time": time}
