aabaf0b2dae2968ede181a3079f6fd0ab46d3756
BHS_drone_team <jglaw2023@gmail.com>
2022-11-29 15:37:33 -0500

    added more servo control

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -139,7 +139,10 @@ class Sandbox(MQTTModule):
             self.has_dropped_3 = False
             self.has_dropped_4 = False
             self.has_dropped_5 = False
-
+        if reset == True and reset_button == 1:
+            self.open_servo_angle(4, 250)
+        if reset == False and reset_button == 1:
+            self.close_servo_angle(4, 125)
 
 
 
@@ -150,12 +153,24 @@ class Sandbox(MQTTModule):
                     {"servo": channel, "action": "open"}
             )
 
+    def open_servo_angle(self, channel, position):
+        self.send_message(
+                    "avr/pcm/set_servo_abs",
+                    {"servo": channel, "absolute": position}
+            )
+
+
     def close_servo(self, channel):
         self.send_message(
                     "avr/pcm/set_servo_open_close",
                     {"servo": channel, "action": "close"}
             )
 
+    def close_servo_angle(self, channel, position):
+        self.send_message(
+                    "avr/pcm/set_servo_abs",
+                    {"servo": channel, "absolute": position}
+            )
     # Blink led for desired iterations with desired wrbg value for specified time interval
     def blink_leds(self, time):
         wrgb = (0,0,51,255)
