106c0793ee813b26a764b737052f12fc66810c37
BHS_drone_team <jglaw2023@gmail.com>
2022-11-30 16:52:41 -0500

    help

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -143,8 +143,8 @@ class Sandbox(MQTTModule):
             self.close_servo_percent(4, 0)
             self.open_servo_percent(5, 0)
         if reset == False and reset_button == 1:
-            self.open_servo_percent(4, 100)
-            self.close_servo_percent(5, 100)
+            self.open_servo_percent(4, 200)
+            self.close_servo_percent(5, 200)
         if reset == True and reset_button == 4:
             self.open_servo(4)
             self.open_servo(5)
