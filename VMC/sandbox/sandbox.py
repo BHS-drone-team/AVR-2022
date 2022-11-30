dc928569555f2c75d6f8007aea3a7f821212fa8c
BHS_drone_team <jglaw2023@gmail.com>
2022-11-30 18:24:49 -0500

    cringe

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -143,7 +143,7 @@ class Sandbox(MQTTModule):
             self.open_servo(4)
             self.open_servo(5)
         if reset == False and reset_button == 1:
-            self.open_servo_percent(4, 99)
+            self.open_servo_percent(4, 1)
             self.close_servo_percent(5, 99)
         if reset == True and reset_button == 4:
             self.open_servo(4)
