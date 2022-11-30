669f6fc988d3363e2ff7a2228712443feb1b97d5
BHS_drone_team <jglaw2023@gmail.com>
2022-11-30 18:20:51 -0500

    a

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -141,7 +141,7 @@ class Sandbox(MQTTModule):
             self.has_dropped_5 = False
         if reset == True and reset_button == 1:
             self.open_servo(4)
-            self.close_servo(5)
+            self.open_servo(5)
         if reset == False and reset_button == 1:
             self.open_servo_percent(4, 99)
             self.close_servo_percent(5, 1)
